3a35a0eda1795f5f70dd330b79157ffff0502ea0
unknown <priyambodobernandiko@gmail.com>
2026-01-28 12:22:25 +0700

    error handling

--- app/(landing)/product/[id]/page.tsx
+++ app/(landing)/product/[id]/page.tsx
@@ -21,7 +21,7 @@ const ProductDetail =  async ({ params }: TPageProps) => {
             <div className="w-full py-7">
                 <h1 className="font-bold text-5xl mb-6">{product.name}</h1>
                 <div className="bg-primary-light rounded-full text-primary py-2 px-6 w-fit mb-5">
-                    {product.category.name}
+                    {product.category?.name ?? "Uncategorized"}
                 </div>
                 <p className="leading-loose mb-8">
                     {product.description}
